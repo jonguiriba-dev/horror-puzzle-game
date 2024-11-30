1afe6c9965c22d3381b766643e9ffd7b4f3edbc9
Jon Guiriba <jon.guiriba@propelleraero.com.au>
2024-11-30 23:32:28 +0800

    a

--- src/components/grid/grid.gd
+++ src/components/grid/grid.gd
@@ -21,9 +21,7 @@ enum HIGHLIGHT_COLORS{
 @onready var threat_highlight_layer :TileMapLayer= $ThreatHighlightLayer
 @onready var debug_highlight_layer :TileMapLayer= $DebugHighlightLayer
 @onready var cursor_layer :TileMapLayer= $CursorLayer
-@onready var prop_layer :TileMapLayer= $PropLayer1
-@onready var prop_layer2 :TileMapLayer= $PropLayer2
-@onready var prop_layer3 :TileMapLayer= $PropLayer3
+@onready var prop_layer :TileMapLayer= $PropLayer
 @onready var astar_grid = AStarGrid2D.new()
 
 var threat_tiles:Array[Vector2i]= []
